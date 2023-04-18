9a3c63f03ed3b8c2164b197966e314fbee458aae
Richard <richard.balakrishnan@syncfusion.com>
2023-04-18 21:42:52 +0530

    config(REPORT-1000): v5.1 changes included

--- src/controls/dynamic-columns/dynamic-columns.js
+++ src/controls/dynamic-columns/dynamic-columns.js
@@ -1,12 +1,12 @@
 /**
- * Product Line Sales - This sample demonstrates the product sales details information based on category and sub category of products report parameters.
+ * Website Visitor Analysis - This sample analyze the user behavior for a fictitious e-commerce website with random data.
  */
-$(function () {
+ $(function () {
     $("#container").boldReportViewer({
         // Specifies the report Web API service URL. It is used to process the reports.
         reportServiceUrl: window.Globals.SERVICE_URL,
         // Specifies the path of the RDL report file
-        reportPath: 'product-line-sales.rdl',
+        reportPath: 'dynamic-columns.rdl',
         toolbarSettings: window.Globals.TOOLBAR_OPTIONS,
         toolBarItemClick: window.Globals.EDIT_REPORT,
         exportItemClick: window.Globals.EXPORT_ITEM_CLICK
