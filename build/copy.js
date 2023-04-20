443a741ab67365f303d123cf487e83459584d809
Richard <richard.balakrishnan@syncfusion.com>
2023-04-20 11:55:43 +0530

    config(REPORT-1000): v5.1 changes

--- build/copy.js
+++ build/copy.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const { cp, mkdir } = require('shelljs');
+const fs = require('fs');
 
 const scripts = {
     common: ['common/ej2-base.min.js', 'common/ej2-data.min.js', 'common/ej2-pdf-export.min.js', 'common/ej2-svg-base.min.js'],
@@ -8,8 +9,13 @@ const scripts = {
 
 const srcDir = 'node_modules/@boldreports/javascript-reporting-controls/Scripts/';
 const destDir = 'scripts/';
+const barcodeDir = './build/templates/extensions/report-item-extensions/';
+const barcodeTeml = {
+    '1D': 'export { EJBarcode };',
+    '2D': 'export { EJQRBarcode };'
+}
 
-gulp.task('copy', function (done) {
+gulp.task('copy', (done) => {
     copyFiles(scripts.common, destDir + 'common');
     copyFiles(scripts.control, destDir + 'data-visualization');
     done();
@@ -21,3 +27,19 @@ function copyFiles(fileArray, dest) {
         cp('-r', srcDir + file, dest);
     });
 };
+
+gulp.task('update-barcode', (done) => {
+    if (fs.existsSync(`${barcodeDir}barcode.reportitem.js`) && fs.existsSync(`${barcodeDir}qrbarcode.reportitem.js`)) {
+        var barcode = fs.readFileSync(`${barcodeDir}barcode.reportitem.js`);
+        var qrbarcode = fs.readFileSync(`${barcodeDir}qrbarcode.reportitem.js`);
+        if (!barcode.includes(barcodeTeml['1D']))
+            fs.writeFileSync(`${barcodeDir}barcode.reportitem.js`, `${barcode} \n ${barcodeTeml['1D']}`);
+        if (!qrbarcode.includes(barcodeTeml['2D']))
+            fs.writeFileSync(`${barcodeDir}qrbarcode.reportitem.js`, `${qrbarcode} \n ${barcodeTeml['2D']}`);
+        done();
+    }
+    else {
+        console.log(`!!!... The Barcode files not found in ${barcodeDir} ...!!!`);
+        process.exit(1);
+    }
+});
